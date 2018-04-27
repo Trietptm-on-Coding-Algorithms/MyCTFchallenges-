cab03117addd19150e6763f193fd3ec3529c75f1
Magdy Moustafa <sakafa_magdy@yahoo.com>
2018-04-27 20:59:17 +0200

    Add files via upload

--- Sources/x_or_what/main.cpp
+++ Sources/x_or_what/main.cpp
@@ -11,26 +11,25 @@ int main(int argc, char *argv[])
     QTextStream(stdout) << "Input The Flag : ";
     QString value = s.readLine();
     QString mykey = "44";
-    //FLAG{wher3_1s_m7_m1nd}
+    //FLAG{where_is_my_mind}
     QChar* input = value.data() ;
     int inputLength = value.length();
     QChar* key = mykey.data();
-    int keyLength   = mykey.length();
 
     char output[inputLength];
     int serial;
     QString final = "";
     for (int i = 0; i < inputLength + 1; ++i)
     {
-        output[i] = input[i].toAscii() ^ key[i % keyLength + 1].toAscii();
+        output[i] = input[i].toAscii() ^ key[0].toAscii();
     }
     for(int i = 0; i < inputLength + 1; ++i)
     {
         serial = output[i];
-        final += QString::number(serial);
+        final += QString::number(serial) + " ";
     }
-
-    if(final == "114761177179119921017051107497195895510710951108012552"){
+    qDebug() << final;
+    if(final == "114 120 117 115 79 67 92 81 70 81 107 93 71 107 89 77 107 89 93 90 80 73 52 "){
         qDebug() << "Correct !!";
     }else{
         qDebug() << "Try Harder !";
